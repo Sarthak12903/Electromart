e94f525a0e273d98eb9cbdd5c516013f13346769
Sarthak12903 <sarthak12903@gmail.com>
2025-01-09 16:22:48 +0530

    Product

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,8 +1,5 @@
+import Products from "../../components/Products/Products";
+
 export default function ProductList() {
-  return (
-    <div className="flex w-[95%] mx-auto mt-4 gap-3">
-      <div className="bg-white flex-[2] rounded-md p-3">asd</div>
-      <div className="bg-white flex-[10] rounded-md p-3">asd</div>
-    </div>
-  );
+  return <Products />;
 }
