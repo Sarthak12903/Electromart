b9496a8896c285920596a250e8352e24f52f78f2
Sarthak12903 <sarthak12903@gmail.com>
2025-01-12 12:17:40 +0530

    reduxUI added

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import ProductList from "./pages/ProductList/ProductList";
 import Footer from "./components/Footer/Footer";
 import useScrollToTop from "./Hooks/Scroll";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 const ScrollToTopWrapper = ({ children }) => {
   useScrollToTop();
   return children;
 };
 export default function App() {
   return (
-    <div className="flex flex-col justify-center items-center select-none">
-      <BrowserRouter>
-        <ScrollToTopWrapper>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/product" element={<ProductList />} />
-          </Routes>
-          <Footer />
-        </ScrollToTopWrapper>
-      </BrowserRouter>
-    </div>
+    <Theme>
+      <div className="flex flex-col justify-center items-center select-none">
+        <BrowserRouter>
+          <ScrollToTopWrapper>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/product" element={<ProductList />} />
+            </Routes>
+            <Footer />
+          </ScrollToTopWrapper>
+        </BrowserRouter>
+      </div>
+    </Theme>
   );
 }
--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,23 +1,12 @@
+import { Slider } from "@radix-ui/themes";
+
 export default function Filters() {
   return (
     <div>
       <h1 className="text-xl font-semibold p-2 border-b-2">Filters</h1>
       <div className="m-2">
         <h3 className="font-semibold">PRICE</h3>
-        <input
-          className="w-full p-2"
-          min={0}
-          max={100000}
-          type="range"
-          list="marker"
-        />
-        <datalist id="marker">
-          <option value="20000"></option>
-          <option value="40000"></option>
-          <option value="60000"></option>
-          <option value="80000"></option>
-          <option value="100000"></option>
-        </datalist>
+        <Slider defaultValue={[25, 75]} className="outline-none" />
       </div>
     </div>
   );
