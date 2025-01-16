5383a727969b0d30b4427dc0bd4409736ab2a4a2
Sarthak12903 <sarthak12903@gmail.com>
2025-01-16 15:33:37 +0530

    Responsiveness added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -295,10 +295,14 @@ export default function Products({
       )}
 
       {["KITCHEN", "SMART"].includes(productType) && (
-        <div className=" flex-[10] grid h-fit bg-white sm:-ml-3 md:ml-3 gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-4 shadow-md p-6">
+        <div className=" flex-[10] grid h-fit bg-white  gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-4 shadow-md p-6">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((value) => (
-              <Link key={value.number} to={"/productDetail"} className="group">
+              <Link
+                key={value.number}
+                to={"/productDetail"}
+                className="group sm:w-[8.8rem] sm:-mx-2 md:mx-2   md:w-auto "
+              >
                 <div className="flex flex-col rounded-md h-fit m-2 md:m-0 sm:w-[8.5rem] md:w-auto cursor-pointer p-2 md:p-6 hover:shadow-xl hover:scale-105 transition-all ease-linear duration-200 ">
                   <div className="w-full ">
                     <img
@@ -307,7 +311,7 @@ export default function Products({
                     />
                   </div>
                   <div className="leading-tight">
-                    <h4 className="line-clamp-2 sm:text-xs group-hover:text-blue-700">
+                    <h4 className="line-clamp-2 sm:text-xs md:text-sm group-hover:text-blue-700">
                       {value.nameDescription}
                     </h4>
                     <p className="text-xs text-slate-700 line-clamp-1">
